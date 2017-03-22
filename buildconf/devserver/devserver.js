605def802bd43f599f61e6cc05cbb6a5ce8664a5
Josh Ouyang <me@joshoy.org>
2017-03-23 01:13:37 +0800

    Proxy for API server

--- buildconf/devserver/devserver.js
+++ buildconf/devserver/devserver.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import path from 'path';
+import proxy from 'express-http-proxy';
 
 const prjRoot = p => path.resolve(__dirname, '../../', p);
 
 const main = async () => {
   const app = express();
   /* app routes */
+  app.use('/api', proxy('localhost:5000'));
   app.use('/static', express.static(prjRoot('./devbuild/')));
   app.get('/*', (req, res) => {
     res.sendFile('index.html', { root: prjRoot('./devbuild/') });
